1984ce59b70c29d968ef32ff3b776bf37fabff8a
skipper <skipper.liu@alo7.com>
2020-12-15 22:57:28 +0800

    python学习

--- base2/module.py
+++ base2/module.py
@@ -1,4 +1,17 @@
 import os
 import sys
 
-sys.path.append("")
+v1 = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(v1)
+res = os.path.join(v1,"base1")
+print(res)
+sys.path.append(res)
+
+import loopTest
+import csv
+
+
+
+
+
+
